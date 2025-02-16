8bd6b749f8dac6975c6262a9a48469c24a66f238
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-02-16 18:52:34 +0530

    Done Testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
       <Navbar />
       <section className="h-[90vh] w-full rounded-md flex sm:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight />
-        <div className="p-4 max-w-7xl mx-auto w-full pt-20 md:pt-0">
-          <h1 className="font-jakarta  sm:text-2xl lg:text-6xl font-semibold text-center text-white">
-            नमस्ते, I'm <span className="font-right bg-clip-text text-transparent bg-gradient-to-b from-emerald-300 to-sky-300">Pradeep Varma</span>
+        <div className="max-w-7xl mx-auto w-full py-20 md:pt-0">
+          <h1 className="p-1 w-full font-jakarta  sm:text-3xl md:text-6xl font-semibold text-center text-white">
+            <span className="sm:text-2xl md:text-4xl">नमस्ते, I'm</span> <span className="font-right bg-clip-text text-transparent bg-gradient-to-b from-emerald-300 to-sky-300">Pradeep Varma</span>
           </h1>
-          <p className="sm:mt-2 md:mt-5 font-medium font-outfit sm:text-base lg:text-xl text-gray-300 max-w-lg text-center mx-auto">
+          <p className="sm:mt-2 md:mt-5 font-medium font-outfit sm:text-sm md:text-xl text-gray-300 max-w-lg text-center mx-auto">
             Student | Full Stack Developer | Cyber Enthusiast
           </p>
         </div>
