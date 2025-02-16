39855699e6071c1204c53bb5ed1b69dc374eb02f
Pradeep Varma <pradeeppenumatsa555@gmail.com>
2025-02-16 18:41:39 +0530

    Testing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
     return (
-        <div className="flex justify-center items-center top-3 fixed w-full z-10">
+        <div className="flex justify-center items-center sm:top-12 md:top-3 fixed w-full z-10">
             <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
                 <a href="#" className="font-outfit px-4 py-1.5 rounded-full text-white/70 text-sm md:text-base lg:text-lg font-medium hover:bg-white/10 hover:text-white transition duration-300" >Home</a>
                 <a href="#" className="font-outfit px-4 py-1.5 rounded-full text-white/70 text-sm md:text-base lg:text-lg font-medium hover:bg-white/10 hover:text-white transition duration-300" >About</a>
